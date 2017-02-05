0705b476e40122b54385b23020743cd165779731
franco83-2 <franco.84@gmail.com>
2017-02-04 23:17:22 -0500

    Done.

--- components/Game.js
+++ components/Game.js
@@ -37,6 +37,10 @@ class Game extends React.Component {
     this.addPancake = this.addPancake.bind(this);
     this.takeItOff = this.takeItOff.bind(this);
   }
+  componentWillMount() {
+    this.setCurrentTime()
+  }
+
 
   render() {
     const { pancakes, burnt, cooked, raw, time } = this.state;
--- components/Pancake.js
+++ components/Pancake.js
@@ -1,9 +1,9 @@
 const React = require('react');
 
 class Pancake extends React.Component {
-  
+
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
-  
+
   // TODO: create a componentWillUnmount() which will clear the interval
 
   updateCounter() {
@@ -59,6 +59,14 @@ class Pancake extends React.Component {
     this.flip = this.flip.bind(this);
     this.takeItOff = this.takeItOff.bind(this);
   }
+
+    componentDidMount() {
+      this.startInterval()
+    }
+
+    componentWillUnmount() {
+      this.cleanUpInterval()
+    }
 
   render() {
     const { timeCooked, flippedAt } = this.state;
